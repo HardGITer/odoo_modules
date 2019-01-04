36adb7cf8f6619f44f9730f5709342e649074556
HardGITer <ksenkong@mail.ru>
2019-01-04 18:34:29 +0300

    fixed calculate_students test

--- local-addons/custom/students/tests/test_queue_job_functions.py
+++ local-addons/custom/students/tests/test_queue_job_functions.py
@@ -1,25 +1,19 @@
 from odoo.tests import common
 from odoo.tests import tagged
-from odoo.addons.queue_job.job import (
-    Job,
-    RETRY_INTERVAL,
-    PENDING,
-    ENQUEUED,
-    STARTED,
-    DONE,
-    FAILED,
-    identity_exact,
-)
 
 
 @tagged('post_install')
 class TestFunctions(common.TransactionCase):
 
-    def setup(self):
-        self.method = self.env['students.person'].calculate_students_count
-
-    def test_new_job(self):
-        test_job = Job(self.env['students.person'].calculate_students_count)
-        self.assertEqual(test_job.func, self.env['students.person'].calculate_students_count)
+    def test_calculate_students_with_delay(self):
+        # self.cr.execute('delete from queue_job')
+        job_instance = self.env['students.person'].with_delay().calculate_students_count()
+        self.assertTrue(job_instance)
+        result = job_instance.perform()
+        self.assertEquals(
+            result,
+            len(self.env['students.person'].search([]))
+        )
         print('queue_job function passed the tests')
+
 
