2e77bc501e3e17dc3949f4e43637fe5569ab9c03
HardGITer <ksenkong@mail.ru>
2019-02-22 17:13:58 +0300

    naming fix

--- local-addons/custom/construction_purchase/models/construction_purchase.py
+++ local-addons/custom/construction_purchase/models/construction_purchase.py
@@ -7,7 +7,7 @@ class Purchase(models.Model):
     @api.onchange('order_line')
     def compute_product_count_in_dock(self):
         Product = self.env['product.product']
-        for product in self.order_line:
-            if product.product_id.count_in_dock - product.product_qty < 0:
+        for line in self.order_line:
+            if line.product_id.count_in_dock - line.product_qty < 0:
                 raise UserError(_("Count of this product in dock less then inputed value"))
-            product.product_id.write({'count_in_dock': product.product_id.count_in_dock - product.product_qty})
+            line.product_id.write({'count_in_dock': line.product_id.count_in_dock - line.product_qty})
